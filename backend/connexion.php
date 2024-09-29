0c939760d97fb3f6b9640c03aa41c79d1ff46403
Tabiseelelwa <esubetabiseelelwa@gmail.com>
2024-09-30 00:14:04 +0100

    mise à jour du mot de passe

--- backend/connexion.php
+++ backend/connexion.php
@@ -9,7 +9,7 @@
 
     try
     {
-        $conn = new PDO("mysql: host=$host; dbname=$db", $username, "");
+        $conn = new PDO("mysql: host=$host; dbname=$db", $username, $pass);
         $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
     }catch(PDOException $m)
     {
